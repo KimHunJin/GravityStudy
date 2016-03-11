e903a32f1280305fc5356f1308b759550e1e74ad
KimMiSun <haruma5011@hanmail.net>
2016-03-11 16:33:49 +0900

    20160311 fragment,roundbutton,listview

---
+++ KMS/MyApplication/app/src/main/java/com_0311_01/example/user/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com_0311_01.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ KMS/MyApplication/app/src/main/java/com_0311_01/example/user/myapplication/NevigationFragment.java
@@ -0,0 +1,13 @@
+package com_0311_01.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NevigationFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_nevigation);
+    }
+}
